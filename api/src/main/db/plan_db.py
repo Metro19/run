3c19a9a2767467be9cd07f22664c4855a0e1fabf
Zack Bamford <40527150+Metro19@users.noreply.github.com>
2023-05-13 15:35:46 -0400

    Added User List commands to Plan backend

--- api/src/main/db/plan_db.py
+++ api/src/main/db/plan_db.py
@@ -7,13 +7,36 @@ Functions to modify and create plans within the database
 
 import logging
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Union
 
 import sqlalchemy
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import Mapped
 
 from api.src.main.db import generic_db
+from api.src.main.db.user_db import User
+
+
+def sep_users(users: str) -> list[str]:
+    """
+    Separate users string into a list of users
+
+    :param users: Users string
+    :return: List of users
+    """
+
+    return users.split("#")
+
+
+def join_users(users: list[str]) -> str:
+    """
+    Join a list of users into a string
+
+    :param users: List of users
+    :return: Users string
+    """
+
+    return "#".join(users)
 
 
 class Plan(generic_db.Base):
@@ -23,20 +46,24 @@ class Plan(generic_db.Base):
 
     ID: Mapped[str] = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
     name: Mapped[str] = sqlalchemy.Column(sqlalchemy.String)
+    description: Mapped[str] = sqlalchemy.Column(sqlalchemy.String)
     date: Mapped[datetime] = sqlalchemy.Column(sqlalchemy.DateTime)
     distance: Mapped[float] = sqlalchemy.Column(sqlalchemy.Float)
     distance_unit: Mapped[str] = sqlalchemy.Column(sqlalchemy.String)
+    users: Mapped[str] = sqlalchemy.Column(sqlalchemy.String)  # user ID separated by "#"
 
     def __repr__(self):
-        return f"Plan: {self.ID} {self.name} {self.date} {self.distance} {self.distance_unit}"
+        return f"Plan: {self.ID} {self.name} {self.description} {self.date} {self.distance} {self.distance_unit} " \
+               f"{sep_users(self.users)}"
 
     def __eq__(self, other):
         # check for same type
         if not isinstance(other, Plan):
             return False
 
-        return self.ID == other.ID and self.name == other.name and self.date == other.date and \
-            self.distance == other.distance and self.distance_unit == other.distance_unit
+        return self.ID == other.ID and self.name == other.name and self.description == other.description and \
+            self.date == other.date and self.distance == other.distance and self.distance_unit == other.distance_unit \
+            and self.users == other.users
 
     def equals_no_id(self, other):
         """
@@ -50,8 +77,8 @@ class Plan(generic_db.Base):
         if not isinstance(other, Plan):
             return False
 
-        return self.name == other.name and self.date == other.date and self.distance == other.distance and \
-            self.distance_unit == other.distance_unit
+        return self.name == other.name and self.description == other.description and self.date == other.date and \
+            self.distance == other.distance and self.distance_unit == other.distance_unit
 
 
 class PlanCommands:
@@ -82,7 +109,7 @@ class PlanCommands:
 
         # create new plan object
         new_plan = Plan(ID=generic_db.create_id("PLAN_"), name=name, date=date, distance=distance,
-                        distance_unit=distance_unit)
+                        distance_unit=distance_unit, users="")
 
         # add to db
         with Session(self.engine) as session:
@@ -108,16 +135,119 @@ class PlanCommands:
         logging.debug(f"Retrieved plan: {p}")
         return p
 
+    def get_user_ids_in_plan(self, plan_id: str) -> Optional[list[str]]:
+        """
+        Get the user IDs in a plan
+
+        :param plan_id: Plan ID to retrieve from
+        :return: List of user IDs, or None if the plan does not exist
+        """
+
+        p: Optional[Plan] = self.retrieve_plan(plan_id)
+
+        # check for no response
+        if p is None:
+            return None
+
+        # return the ids
+        return sep_users(p.users)
+
+    def get_user_objects_in_plan(self, plan_id: str) -> Optional[list[User]]:
+        """
+        Get the user objects in the plan
+
+        :param plan_id: Plan ID to get users from
+        :return: Users in the plan
+        """
+
+        p: Optional[Plan] = self.retrieve_plan(plan_id)
+
+        # check for no response
+        if p is None:
+            return None
+
+        users = []
+
+        with Session(self.engine) as session:
+
+            # get all users
+            for user_id in sep_users(p.users):
+                curr_user: Optional[User] = session.get(User, user_id)
+
+                # verify user
+                if curr_user is not None:
+                    users.append(curr_user)
+
+        logging.debug(f"Retrieved users in plan: {users}")
+        return users
+
+    def add_users_from_plan(self, plan_id: str, users: Union[list[User], list[str]]) -> Optional[Plan]:
+        """
+        Add users to a plan
+
+        :param plan_id: Plan ID to modify
+        :param users: Users to add, or user IDs to add
+        :return: Plan with users added
+        """
+
+        p: Optional[Plan] = self.retrieve_plan(plan_id)
+
+        # check for no response
+        if p is None:
+            return None
+
+        # format as list of user ids
+        if type(users[0]) == User:
+            users = [user.ID for user in users]
+
+        # get user list and add users
+        user_list = sep_users(p.users)
+        user_list += users
+
+        # remove duplicates
+        user_list = list(set(user_list))
+
+        # update plan
+        self.modify_plan(plan_id, p.name, p.date, p.distance, p.distance_unit, join_users(user_list))
+
+    def remove_users_from_plan(self, plan_id: str, users: Union[list[User], list[str]]) -> Optional[Plan]:
+        """
+        Remove users from a plan
+
+        :param plan_id: Plan ID to modify
+        :param users: Users to remove, or user IDs to remove
+        :return: Plan with users removed
+        """
+
+        p: Optional[Plan] = self.retrieve_plan(plan_id)
+
+        # check for no response
+        if p is None:
+            return None
+
+        # format as list of user ids
+        if type(users[0]) == User:
+            users = [user.ID for user in users]
+
+        # get user list and remove users with list comprehension
+        user_list = sep_users(p.users)
+        user_list = [user for user in user_list if user not in users]
+
+        # update plan
+        self.modify_plan(plan_id, p.name, p.date, p.distance, p.distance_unit, join_users(user_list))
+
     def modify_plan(self, plan_id: str, new_name: str, new_date: datetime, new_distance: float,
-                    new_distance_unit: str) -> Optional[Plan]:
+                    new_distance_unit: str, new_user_list: str) -> Optional[Plan]:
         """
         Modify a plan
 
+        :param new_user_list:
         :param plan_id: ID of the plan
         :param new_name: New name of the plan
         :param new_date: New date of the plan
         :param new_distance: New distance of the plan
         :param new_distance_unit: New distance unit of the plan
+        :param new_user_list: New user list of the plan
         :return: Modified plan object
         """
 
@@ -134,6 +264,7 @@ class PlanCommands:
             p.date = new_date
             p.distance = new_distance
             p.distance_unit = new_distance_unit
+            p.users = new_user_list
 
             session.commit()
 
