01e427da24a1432885bc01175e43a46290c5ed3b
Zack Bamford <40527150+Metro19@users.noreply.github.com>
2023-05-21 23:05:52 -0400

    Write log-in system for header

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,26 @@
-import { MantineProvider, Text } from '@mantine/core';
+import {createStyles, MantineProvider, rem, Text} from '@mantine/core';
+import {Header} from "./navbar/Header";
+
+export const HEADER_HEIGHT = 75;
+
+const useStyles = createStyles((theme) => ({
+    all: {
+        marginTop: rem(HEADER_HEIGHT),
+    },
+
+    text: {
+        color: "white",
+    }
+}))
 
 export default function App() {
+  const {classes, theme} = useStyles();
   return (
       <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme:'dark'}}>
-        <Text>Welcome to Mantine!</Text>
+          <Header />
+          <div className={classes.all}>
+              <Text className={classes.text} >Welcome to Mantine!</Text>
+          </div>
       </MantineProvider>
   );
 }
---
+++ ui/src/navbar/Header.tsx
@@ -0,0 +1,95 @@
+import {Button, Center, rem, createStyles, Skeleton, Notification} from "@mantine/core";
+import {HEADER_HEIGHT} from "../App";
+import {API_URL} from "../api_interface/api_setup";
+import {useEffect, useState} from "react";
+import {Simulate} from "react-dom/test-utils";
+
+
+// styles
+const useStyles = createStyles((theme) => ({
+    header: {
+        position: "fixed",
+        top: 0,
+        width: "100%",
+
+        backgroundColor: theme.primaryColor,
+        height: rem(HEADER_HEIGHT),
+    },
+
+    title: {
+        margin: "auto",
+        position: "absolute",
+
+        left: `${theme.spacing.md}`,
+    },
+
+    user_menu: {
+        margin: "auto",
+        position: "absolute",
+
+        right: `${theme.spacing.md}`,
+    },
+
+    button: {
+        backgroundColor: theme.colors.blue[6],
+    }
+}))
+
+
+
+function UserMenu() {
+    const {classes, theme} = useStyles();
+
+    // data storage
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    // get user data
+    useEffect(() => {
+        // check if need to fetch
+        if (userData !== null || error !== null) return;
+
+        fetch(API_URL + "/user/info", {headers: {Authorization: "Bearer " + localStorage.getItem("token")}})
+            .then((response) => response.json())
+            .then((response) => {setUserData(response.username); setError(null);})
+            .catch((error) => {setError(error); setUserData(null);})
+            .finally(() => setLoading(false));
+    })
+
+    // return information
+    return (
+        <div>
+            <Skeleton visible={loading}>
+                {/* On error, display log in page */}
+                {error &&
+                <div>
+                    <Button className={classes.button} onClick={() => window.location.href = "/login"}>Login</Button>
+                </div>
+                }
+                {userData && <Button className={classes.button}>{userData}</Button>}
+            </Skeleton>
+        </div>
+    )
+}
+
+export function Header() {
+    const {classes, theme} = useStyles();
+    return (
+        <div className={classes.header}>
+            <Center h={HEADER_HEIGHT} className={classes.title}>
+                <h1><i>R</i>un</h1>
+            </Center>
+
+            <Center h={HEADER_HEIGHT} className={classes.user_menu}>
+                {/* Only try displaying menu if user has a token saved to browser */}
+                {localStorage.getItem("token") !== null && <UserMenu/>}
+
+                {/* If user has no token, display login button */}
+                {localStorage.getItem("token") === null &&
+                    <Button className={classes.button} onClick={() => window.location.href = "/login"}>Login</Button>
+                }
+            </Center>
+        </div>
+    )
+}
